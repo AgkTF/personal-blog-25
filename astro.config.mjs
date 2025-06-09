d70fa4570bdb73f4b02a82dfb411808aa8619390
Ahmed Agk <ahmed.agk22@gmail.com>
2025-06-09 14:09:26 +0300

    Installed netlify adapter for Astro.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: netlify(),
 });
